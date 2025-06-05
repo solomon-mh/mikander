5135e01b53210f51ff6a923a83ab7ecec20935aa
solomon-mh <solomon.muhye.wd@gmail.com>
2025-06-05 10:48:24 +0300

    Update title in metadata from "Converso" to "Mikander"

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const bricolage = Bricolage_Grotesque({
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
+  title: "Mikander",
   description: "Real-time AI Teaching Platform",
 };
 
