4bb749b06787116e48fe07a97f5196fe85949905
solomon-mh <solomon.muhye.wd@gmail.com>
2025-06-11 10:52:57 +0300

    Refactor SubjectFilter component for improved readability and consistency

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import {
   Select,
@@ -8,33 +9,34 @@ import {
   SelectValue,
 } from "./ui/select";
 import { subjects } from "@/constants";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
-// import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/search-param-utils";
 
 const SubjectFilter = () => {
+  const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
   const query = searchParams.get("subject") || "";
-
   const [subject, setSubject] = useState(query);
 
-  // useEffect(() => {
-  //     let newUrl = "";
-  //     if (subject === "all") {
-  //         newUrl = removeKeysFromUrlQuery({
-  //             params: searchParams.toString(),
-  //             keysToRemove: ["subject"],
-  //         });
-  //     } else {
-  //         newUrl = formUrlQuery({
-  //             params: searchParams.toString(),
-  //             key: "subject",
-  //             value: subject,
-  //         });
-  //     }
-  //     router.push(newUrl, { scroll: false });
-  // }, [subject]);
+  useEffect(() => {
+    let newUrl = "";
+    if (subject === "all") {
+      newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keysToRemove: ["subject"],
+      });
+      return router.push(`${pathname}${newUrl}`, { scroll: false });
+    } else {
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "subject",
+        value: subject,
+      });
+      return router.push(`${pathname}${newUrl}`, { scroll: false });
+    }
+  }, [pathname, router, searchParams, subject]);
 
   return (
     <Select onValueChange={setSubject} value={subject}>
