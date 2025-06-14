befd55f8aaa5371c9e200245182a533a4bc0fde8
solomon-mh <solomon.muhye.wd@gmail.com>
2025-06-14 13:55:16 +0300

    Refactor Page component to render popular companions and recently completed sessions

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
+// import {
+//   getAllCompanions,
+//   getRecentSessions,
+// } from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+  // const companions = await getAllCompanions({ limit: 3 });
+  const companions = [];
+  // const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
-    <div>Page</div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
 
-export default Page
+      <section className="home-section">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
