9c4a46036f216f561b84ab1c1093bb1ada128cd4
solomon-mh <solomon.muhye.wd@gmail.com>
2025-06-15 12:50:19 +0300

    Add image remote patterns configuration for external image hosting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com", // your image host
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
