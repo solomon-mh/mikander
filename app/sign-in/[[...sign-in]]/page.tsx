739ed9158e48dd0a95626fbd81c257b87569a20f
solomon-mh <solomon.muhye.wd@gmail.com>
2025-06-29 14:03:28 +0300

    Format Page component for consistent styling and improved readability

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,9 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-    return <main className="flex items-center justify-center">
-        <SignIn />
+  return (
+    <main className="flex items-center justify-center">
+      <SignIn />
     </main>
+  );
 }
