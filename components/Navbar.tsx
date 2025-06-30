a797ff415a9a295a7da3148484cf7fdcec325e87
solomon-mh <solomon.muhye.wd@gmail.com>
2025-06-30 14:05:14 +0300

    Refactor Navbar component to improve readability and handle redirect logic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,39 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import NavItems from "@/components/NavItems";
+import { useSearchParams } from "next/navigation";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <Link href="/">
-                <div className="flex items-center gap-2.5 cursor-pointer">
-                    <Image
-                        src="/images/logo.svg"
-                        alt="logo"
-                        width={46}
-                        height={44}
-                    />
-                </div>
-            </Link>
-            <div className="flex items-center gap-8">
-                <NavItems />
-                <SignedOut>
-                    <SignInButton>
-                        <button className="btn-signin">Sign In</button>
-                    </SignInButton>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-            </div>
-        </nav>
-    )
-}
+  const searchParams = useSearchParams();
+  const redirectTo = searchParams.get("redirectTo") || "/x";
+  console.log(redirectTo);
 
-export default Navbar
+  return (
+    <nav className="navbar">
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
+        </div>
+      </Link>
+      <div className="flex items-center gap-8">
+        <NavItems />
+        <SignedOut>
+          <SignInButton
+            forceRedirectUrl={`$http://localhost:3000/${redirectTo}`}
+            fallbackRedirectUrl={`$http://localhost:3000/${redirectTo}`}
+          >
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
